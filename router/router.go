fb20c93976943bda40bf50682be31c6441437214
qiapeng921 <qiuapeng921@163.com>
2020-01-17 18:49:47 +0800

    update

--- router/router.go
+++ router/router.go
@@ -13,6 +13,8 @@ func init() {
 	server.Group("/", func(group *ghttp.RouterGroup) {
 		group.ALL("/", new(controller.IndexController), "Index")
 	})
+	// 某些浏览器直接请求favicon.ico文件，特别是产生404时
+	server.SetRewrite("/favicon.ico", "/resource/image/favicon.ico")
 
 	server.Group("/api", func(group *ghttp.RouterGroup) {
 		group.Middleware(middleware.CORS)
