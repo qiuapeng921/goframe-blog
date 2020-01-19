0ecb301efb3356966abba0e85adf3cbe93a6b529
qiapeng921 <qiuapeng921@163.com>
2020-01-19 18:25:36 +0800

    update

---
+++ app/controller/admin/role.go
@@ -0,0 +1,71 @@
+package admin
+
+import (
+	"blog/app/controller"
+	"blog/app/model/role"
+	"fmt"
+	"github.com/gogf/gf/util/gconv"
+)
+
+type RoleController struct {
+	controller.Controller
+}
+
+func (c *RoleController) List() {
+	result, err := role.Model.Limit(0, 10).All()
+	if err != nil {
+		c.ResponseFail(c.Request, err.Error())
+	}
+	c.ResponseSuccess(c.Request, result)
+}
+
+func (c *RoleController) Info() {
+	id := c.Request.Get("id")
+	result, err := role.FindOne("id", id)
+	if err != nil {
+		c.ResponseFail(c.Request, err.Error())
+	}
+	c.ResponseSuccess(c.Request, result)
+}
+
+func (c *RoleController) Save() {
+	params := c.Request.GetMap()
+	roleName := params["role_name"]
+	roleDesc := params["role_desc"]
+	var roleEntity role.Entity
+	roleEntity.RoleName = gconv.String(roleName)
+	roleEntity.RoleDesc = gconv.String(roleDesc)
+	result, err := role.Insert(roleEntity)
+	if err != nil {
+		c.ResponseFail(c.Request, err.Error())
+	}
+	num ,_ := result.LastInsertId()
+	c.ResponseSuccess(c.Request, num)
+}
+
+func (c *RoleController) Update() {
+	params := c.Request.GetMap()
+	id := params["id"]
+	roleName := params["role_name"]
+	roleDesc := params["role_desc"]
+	_, err := role.FindOne("id", id)
+	if err != nil {
+		c.ResponseFail(c.Request, err.Error())
+	}
+	result, err2 := role.Model.Where("id", id).Data("role_name", roleName, "role_desc", roleDesc).Update()
+	if err2 != nil {
+		c.ResponseFail(c.Request, err2.Error())
+	}
+	num ,_ := result.RowsAffected()
+	c.ResponseSuccess(c.Request, num)
+}
+
+func (c *RoleController) Delete() {
+	id := c.Request.Get("id")
+	result, err := role.Update("status", 1, "id", id)
+	fmt.Println(gconv.String(result))
+	if err != nil {
+		c.ResponseFail(c.Request, err.Error())
+	}
+	c.ResponseSuccess(c.Request, result)
+}
--- router/router.go
+++ router/router.go
@@ -38,15 +38,27 @@ func init() {
 	// TODO ----------------------后台模块接口--------------------------------
 	server.Group("/admin", func(group *ghttp.RouterGroup) {
 		group.Middleware(middleware.CORS)
-		group.Group("/auth", func(group *ghttp.RouterGroup) {
-			group.POST("/login", new(admin.AuthController), "Login")
-		})
-		group.POST("/auth/logout", new(admin.AuthController), "LogOut")
-		group.Group("/manage", func(group *ghttp.RouterGroup) {
-			group.Middleware(middleware.AdminAuth)
-			group.POST("/info", new(admin.ManageController), "Info")
-			group.POST("/list", new(admin.ManageController), "List")
-		})
-	})
+		group.POST("/auth/login", new(admin.AuthController), "Login")
 
+		group.Group("/", func(group *ghttp.RouterGroup) {
+			group.Middleware(middleware.AdminAuth)
+			group.POST("/auth/logout", new(admin.AuthController), "LogOut")
+			// 管理员
+			group.Group("/manage", func(group *ghttp.RouterGroup) {
+				group.POST("/info", new(admin.ManageController), "Info")
+				group.POST("/list", new(admin.ManageController), "List")
+			})
+			// 角色
+			group.Group("/role", func(group *ghttp.RouterGroup) {
+				group.POST("/list", new(admin.RoleController), "List")
+				group.POST("/info/{id}", new(admin.RoleController), "Info")
+				group.POST("/save", new(admin.RoleController), "Save")
+				group.POST("/update/{id}", new(admin.RoleController), "Update")
+				group.POST("/delete/{id}", new(admin.RoleController), "Delete")
+			})
+			// 权限
+
+		})
+
+	})
 }
